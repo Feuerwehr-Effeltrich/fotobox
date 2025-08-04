f6108ac2a41e30fa9294906bd7d801f6e2f8cd5f
Johannes Dittrich <johannes.dittrich@fau.de>
2025-08-04 21:41:49 +0200

    more

--- src/videofeed.py
+++ src/videofeed.py
@@ -1,6 +1,8 @@
 import cv2
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(4)
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 def frames():
     while True:
